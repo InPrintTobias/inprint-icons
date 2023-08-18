1c2028d829133f7fd06e2fcb93a719e5b99ffea4
InPrintTobias <to@inprint.dk>
2023-08-18 11:00:45 +0000

    Added the righhht

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import ChevronDown from './icons/ChevronDown.vue';
+import ChevronUp from './icons/ChevronUp.vue';
+import ChevronRight from './icons/ChevronRight.vue';
+import ChevronLeft from './icons/ChevronLeft.vue';
+
+export { 
+    ChevronDown,
+    ChevronUp,
+    ChevronRight,
+    ChevronLeft,
+}
